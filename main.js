b4eb20dcd7cbfad740937fbeb87a912d407ed7d8
Lucas Reade <lucas.reade@yahoo.com>
2022-08-03 16:06:30 +0100

    Updates to design

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// const headerEl = document.getElementById('header');
+
+
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+// window.onscroll = function() {myFunction()};
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
